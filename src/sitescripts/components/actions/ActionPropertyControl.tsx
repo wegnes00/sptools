0eee57d8ab300c45ad94c4a1386397043d4c1788
Petersen <apetersen@skylinetechnologies.com>
2018-08-15 22:26:13 -0500

    added dropdown support for choice type

--- src/sitescripts/components/actions/ActionPropertyControl.tsx
+++ src/sitescripts/components/actions/ActionPropertyControl.tsx
@@ -4,7 +4,20 @@ import hub from '../../hub/hub';
 import "./ActionPropertyControl.scss";
 export default class ActionPropertyControl extends React.PureComponent<ActionPropertyControlProps, {}> {
     onChange = (e) => {
+        console.log("CHANGE", e);
         hub.trigger("actions:updateProperty", this.props.parentActionId, this.props.property.id, e.currentTarget.value || "");
+    }
+    renderInput = () => {
+        let property = this.props.property;
+        if (property.type === "choice") {
+            return(
+                <select onChange={this.onChange} value={property.value}>
+                    {property.choices.map(choice => <option>{choice}</option>)}
+                </select>
+            )
+        } else {
+            return <input value={property.value} onChange={this.onChange} />
+        }
     }
     render() {
         let property = this.props.property;
@@ -12,7 +25,7 @@ export default class ActionPropertyControl extends React.PureComponent<ActionPro
             <div className='property-control'>
                 <label>{property.title}</label>
                 <br />
-                <input value={property.value} onChange={this.onChange} />
+                {this.renderInput()}
                 <br/>
                 <label>Type:{property.type} - {property.description}</label>
 
