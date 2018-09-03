ecf0c0fdb223c96fb9a3500529c780536c4c122d
Otto <sotto@skylinetechnologies.com>
2018-09-03 13:27:12 -0500

    updated reactions for theme pallette changes

--- src/sitethemes/hub/reactions.ts
+++ src/sitethemes/hub/reactions.ts
@@ -1,14 +1,23 @@
 import hub from "./hub";
 
+let handleThemeColorUpdate = function(newThemeValue) {
+    hub.state.theme.set({ [newThemeValue.themeKey]: newThemeValue.themeValue }).now();
+    hub.cacheState();  
+}
+
+let handleThemeNameUpdate = function(name) {
+    name.trim();
+    hub.state.set({ name }).now();
+    hub.cacheState();  
+}
+
 const handleJSONUpdate = function(json) {
     if (json) {
         hub.state.set({ json }).now();
         hub.cacheState();
     }
-} 
-const handleUpdateProperty = function(propertyId, value) {
-    console.log("Handle Update Property", propertyId, value);
 }
 
-hub.on('actions:updateProperty', handleUpdateProperty);
 hub.on("json:update", handleJSONUpdate);
+hub.on("theme:colorUpdate", handleThemeColorUpdate);
+hub.on("theme:nameUpdate", handleThemeNameUpdate);
