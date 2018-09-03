55d8c348b9a4bb1e9606eeb202b542e3a9ef8f21
Otto <sotto@skylinetechnologies.com>
2018-09-03 13:26:56 -0500

    created theme interface and set defaultState

--- src/sitethemes/data/interfaces.ts
+++ src/sitethemes/data/interfaces.ts
@@ -1,11 +1,9 @@
 
 export interface Theme {
-    name: string,
     primary: string,
     secondary: string,
     tertiary: string,
-    accent: string,
-    advanced: JsonSchema
+    accent: string
 }
 
 export interface JsonSchema {
--- src/sitethemes/hub/defaultState.ts
+++ src/sitethemes/hub/defaultState.ts
@@ -3,11 +3,19 @@ import { FreezerObject } from "./hub";
 import themeJson from "./themeJson";
 
 export interface ApplicationState extends FreezerObject{
-    theme?: Theme,
+    name: string,
+    theme: Theme,
     json: string
 }
 
 let defaultState: ApplicationState = {
+    name: "Skyline Spark",
+    theme: {
+        primary: "#E6003E",
+        secondary: "#E6003E",
+        tertiary: "#4B848E",
+        accent: "#719D64"
+    },
     json: themeJson
 }
 export default defaultState;
