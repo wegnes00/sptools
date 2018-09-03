b424fd3fed49b2205fedac03572d884cade6c60a
Otto <sotto@skylinetechnologies.com>
2018-09-03 13:25:51 -0500

    cleaned up entry and added office ui fabric react for components

--- src/entry.ts
+++ src/entry.ts
@@ -2,9 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./sitescripts/hub/reactions";
 import "./sitethemes/hub/reactions";
-
-// import SiteScriptApp from "./sitescripts/SiteScriptApp";
 import App from "./sptoolsApp/App";
-
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
